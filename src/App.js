05d2118031cbc3d18287e5c48163e696319f9bc5
ntlokoo <Odwa.Ntloko@vodacom.co.za>
2020-04-08 10:51:21 +0200

    Created React App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import SimpleMap from './Components/MapComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <SimpleMap/>
+    </div>
+  );
+}
+
+export default App;
