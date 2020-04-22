0a6fd025685031776c061e41a0c1b0f0e21819a3
ntlokoo <Odwa.Ntloko@vodacom.co.za>
2020-04-22 11:57:15 +0200

    Made components more re-usable/presentational

--- src/Components/MapComponent.js
+++ src/Components/MapComponent.js
@@ -1,20 +1,8 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
-import Marker from './MarkerComponent';
 
 
-class SimpleMap extends Component {
-
-  static defaultProps = {
-    center: {
-      lat: -29.087217,
-      lng: 26.154898
-    },
-    zoom: 5.8
-  };
-
-  
- 
+class SimpleMap extends Component { 
   render() {
     return (
       <div style={{ height: '95vh', width: '100%' }}>
--- src/Components/VisualizerComponent.js
+++ src/Components/VisualizerComponent.js
@@ -3,69 +3,22 @@ import SimpleMap from './MapComponent';
 import VisualizerStyles from '../StyleComponents/VisualizerStyles';
 
 
-class Visualizer extends Component {
-  
-  constructor(props){
-		super(props);
-	
-		this.state = { 
-      data: {}, 
-      global_data : {},
-      center : {
-        lat: -29.087217,
-        lng: 26.154898
-      }, 
-      zoom: 6.2
-		}
-  }
-  
-  componentDidMount = () => {
-
-
-    let postOptions = {};
-    
-    postOptions.method = 'GET';
-    
-    postOptions.headers = {};
-    postOptions.headers['Content-type'] = 'application/json';
-   
-	// Api Call for local stats
-    fetch('https://api.covid19api.com/live/country/south-africa/status/confirmed', postOptions)
-        .then(res => res.json())
-        .then((data) => {
-		this.setState({data:data[data.length-1]})
-		console.log(data[data.length-1])
-
-
-    }).catch(console.log())
-
-    // Global Stats API Call
-    fetch('https://api.covid19api.com/summary', postOptions)
-        .then(res => res.json())
-        .then((data) => {
-        this.setState({global_data:data.Global})
-        console.log(data.Global)   
-        }).catch(console.log)
-
-  
-  }
-  
- 
+class Visualizer extends Component {	
   render() {
     return (
       <VisualizerStyles>
         <h1> The Rona Visualizer</h1>
-        <h2> Country  - <b> {this.state.data.Country} </b></h2>
-          <h2> Confirmed Cases : <b style = {confirmed}> {this.state.data.Confirmed} </b> Active Cases : <b>{this.state.data.Active}</b> Recovered Cases : <b  style = {recovered}>{this.state.data.Recovered } </b> Deaths : <b  style = {death}> {this.state.data.Deaths}</b></h2> 
+        <h2> Country  - <b> {this.props.country} </b></h2>
+          <h2> Confirmed Cases : <b style = {confirmed}> {this.props.confirmed} </b> Active Cases : <b>{this.props.active}</b> Recovered Cases : <b  style = {recovered}>{this.props.recovered } </b> Deaths : <b  style = {death}> {this.props.deaths}</b></h2> 
 
           <div style={{ height: '50%', width: '100%' }}>
-          <SimpleMap center = { this.state.center}
-            zoom = {this.state.zoom}
+          <SimpleMap center = { this.props.center}
+            zoom = {this.props.zoom}
             />
           </div>
 
           <h2> Global Stats </h2>
-          <h2> Total Confirmed Cases : <b  style = {confirmed}> {this.state.global_data.TotalConfirmed}</b>  Total Recovered Cases : <b style = {recovered}>{this.state.global_data.TotalRecovered } </b> Total Deaths : <b  style = {death}> {this.state.global_data.TotalDeaths}</b></h2> 
+          <h2> Total Confirmed Cases : <b  style = {confirmed}> {this.props.global_confirmed}</b>  Total Recovered Cases : <b style = {recovered}>{this.props.global_recovered } </b> Total Deaths : <b  style = {death}> {this.props.global_deaths}</b></h2> 
     </VisualizerStyles>
 
     );
