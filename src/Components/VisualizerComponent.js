ee07ec293d7082aa1fd2829307eaaeac203735ee
ntlokoo <Odwa.Ntloko@vodacom.co.za>
2020-04-30 18:39:21 +0200

    made a minor change to the heading

--- src/Components/VisualizerComponent.js
+++ src/Components/VisualizerComponent.js
@@ -8,7 +8,7 @@ class Visualizer extends Component {
     return (
       <VisualizerStyles>
         <h1> The Rona Visualizer</h1>
-    <h2> Country  - <b> {this.props.country} </b> Tests Conducted: <b> {this.props.tests}</b></h2>
+    <h2> Country  - <b> {this.props.country} </b> &nbsp; | &nbsp; Tests Conducted: <b> {this.props.tests}</b></h2>
           <h2> Confirmed Cases : <b style = {confirmed}> {this.props.confirmed} </b> Active Cases : <b>{this.props.active}</b> Recovered Cases : <b  style = {recovered}>{this.props.recovered } </b> Deaths : <b  style = {death}> {this.props.deaths}</b></h2> 
           <h2 style = {lockdown} > Note: South Africa is currently on <b  style = {confirmed}> LEVEL 4 </b> of the lockdown restrictions  </h2> 
 
@@ -18,8 +18,9 @@ class Visualizer extends Component {
             />
           </div>
 
-          <h2> Global Stats </h2>
-          <h2> Total Confirmed Cases : <b  style = {confirmed}> {this.props.global_confirmed}</b>  Total Recovered Cases : <b style = {recovered}>{this.props.global_recovered } </b> Total Deaths : <b  style = {death}> {this.props.global_deaths}</b></h2> 
+            <h2> Global Stats </h2>
+            <h2> Total Confirmed Cases : <b  style = {confirmed}> {this.props.global_confirmed}</b>  Total Recovered Cases : <b style = {recovered}>{this.props.global_recovered } </b> Total Deaths : <b  style = {death}> {this.props.global_deaths}</b></h2> 
+         
     </VisualizerStyles>
 
     );
@@ -38,10 +39,16 @@ const recovered= {
 	color: '#76ff54'
 };
 
-const lockdown= {
+const lockdown = {
   fontSize: '20px',
   fontWeight: '200'
 };
+
+const globals = {
+  backgroundColor: '#a9a9a9',
+  fontColor: '#fff'
+
+}
  
  
 export default Visualizer;
