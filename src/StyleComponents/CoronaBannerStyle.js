d0f92135ebc494d07375887b07b0264ac3c059bf
ntlokoo <Odwa.Ntloko@vodacom.co.za>
2020-05-08 15:15:58 +0200

    Created corona styling component

---
+++ src/StyleComponents/CoronaBannerStyle.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+import breakpoint from 'styled-components-breakpoint';
+
+const CoronaBanner = styled.div`
+   h4 {
+    font-size: 15px;
+    color: #fff;
+    background-color: #008709;
+    padding-top: 25px;
+    padding-bottom: 25px;
+    text-align: center;
+    font-weight: 150;
+   }
+
+
+  ${breakpoint('tablet')`
+
+  h4 {
+    font-size: 15px;
+    color: #fff;
+    background-color: #008709;
+    padding-top: 25px;
+    padding-bottom: 25px;
+    text-align: center;
+    font-weight: 150;
+   }
+
+
+`}
+
+`;
+export default CoronaBanner;
